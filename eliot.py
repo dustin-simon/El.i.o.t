62d377608747be996912c22a44032e489167572d
dustin-simon <smn.dustin@gmail.com>
2020-07-12 18:07:42 +0200

    create start file and package with version

---
+++ eliot.py
@@ -0,0 +1,9 @@
+import eliot
+
+nameLine = " El.i.o.t - Electronics Input Output Tool ( v" + eliot.__version__+" ) "
+separator = ""
+
+for i in range(len(nameLine)):
+    separator += "="
+
+print("\n"+separator+"\n"+nameLine+"\n"+separator+"\n")
