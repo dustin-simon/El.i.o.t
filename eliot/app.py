3355843570587366c6ccca2d2d92182ec9494e20
dustin-simon <smn.dustin@gmail.com>
2020-07-14 20:58:45 +0200

    create popup with shutdown functionality

--- eliot/app.py
+++ eliot/app.py
@@ -3,7 +3,18 @@
 El.i.o.t - Electronics Input Output Tool
 ========================================
 """
+import os
+
 from kivy.app import App
 
+from eliot.ui.popup.shutdown import Shutdown as ShutdownPopup
+
 class EliotApp(App):
-    pass
+
+    MAIN_COLOR = 0,0,0.5,1
+    
+    def on_power_btn_pressed(self):
+        ShutdownPopup().open()
+
+    def shutdown(self):
+        os.system("sudo shutdown now")
---
+++ eliot/ui/popup/shutdown.py
@@ -0,0 +1,7 @@
+from kivy.lang import Builder
+from kivy.uix.popup import Popup
+
+Builder.load_file("eliot/ui/popup/shutdown.kv")
+
+class Shutdown(Popup):
+    pass
